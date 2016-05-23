ab9d1749358879589566bfc007e07afa88b5de36
Vince Bloise <vbloise3@verizon.net>
2016-05-23 19:57:59 -0400

    Added queue sender index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+/**
+ * Created by vincebloise on 5/23/16.
+ */
+var AWS = require('aws-sdk');
+var uuid = require('node-uuid');
+var config = require('./config.json');
+var sqs = new AWS.SQS({
+    "region": "us-west-2"
+});
+
+if (process.argv.length !== 3) {
+    console.log('URL missing');
+    process.exit(1);
+}
+
+var id = uuid.v4();
+var body = {
+    "id": id,
+    "url": process.argv[2]
+};
+
+sqs.sendMessage({
+    "MessageBody": JSON.stringify(body),
+    "QueueUrl": config.QueueUrl
+}, function(err) {
+    if (err) {
+        console.log('error', err);
+    } else {
+        console.log('PNG will be soon available at http://' + config.Bucket + '.s3-website-us-west-2.amazonaws.com/' + id + '.png');
+    }
+});
