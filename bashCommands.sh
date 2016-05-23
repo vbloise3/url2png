8cb75553319c1157dae9103d7d9c1f4026771189
Vince Bloise <vbloise3@verizon.net>
2016-05-23 19:50:44 -0400

    Corrected s3 bucket create command

--- bashCommands.sh
+++ bashCommands.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 ##create an s3 bucket with web hosting enabled
-aws s3 mb s3L//url2png-vincebloise
+aws s3 mb s3://url2png-vincebloise
 aws s3 website s3://url2png-vincebloise --index-document index.html --error-document error.html
